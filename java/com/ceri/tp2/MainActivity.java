c7a55092fa5aac3adb0cb8498cbcb6a46404cbad
Motyak <anfossotommy@hotmail.fr>
2020-02-19 21:56:23 +0100

    passage a androidx

--- java/com/ceri/tp2/MainActivity.java
+++ java/com/ceri/tp2/MainActivity.java
@@ -3,17 +3,18 @@ package com.ceri.tp2;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
--- java/com/ceri/tp2/WineActivity.java
+++ java/com/ceri/tp2/WineActivity.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class WineActivity extends AppCompatActivity {
 
